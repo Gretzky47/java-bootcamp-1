07d37c9bb3576020552447375163c51bd0bacd68
Maz4kar <makanor@gmail.com>
2021-09-14 13:07:14 -0500

    commit

--- Week2/src/com/coderscampus/week2/Week2Example.java
+++ Week2/src/com/coderscampus/week2/Week2Example.java
@@ -1,32 +1,16 @@
 package com.coderscampus.week2;
 
-import java.util.Scanner;
-
 public class Week2Example {
 
 	public static void main (String[] args) {
+		int age = 19;
 		
-		// This is just for teaching purposes ///////
-		//  with respect to imports / packages     //
-		// java.lang is by default imported for us //
-		java.lang.String aString = "Hello World";  //
-		String anotherString = "Another String";   //
-		/////////////////////////////////////////////
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Type in your name: ");
-		String name = scanner.nextLine();
-		System.out.println("Type in your age: ");
-		String age = scanner.nextLine();
-		
-		if (name.equals("Trevor") 
-				|| age.equals("36")
-				|| name.equals("TJ")) {
-			System.out.println("Holy cow it's me!");
+		if (age >= 19) {       
+			System.out.println("You are an adult");
 		} else {
-			System.out.println("Hello: " + name);
+			System.out.println("You are NOT an adult");
 		}
 		
-		scanner.close();
+		System.out.println("Your age is " + age);
 	}
 }
